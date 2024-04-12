79c6da93fb8213b8f0ac1f17cc760411c70580f1
nesh8383 <166833846+nesh8383@users.noreply.github.com>
2024-04-12 10:06:18 -0400

    Add files via upload

---
+++ P1LAB1_Williams,Denishia.py
@@ -0,0 +1,9 @@
+#Denishia Williams
+#April 11, 2024
+#P1LAB1 Instructions
+#This assigments teaches us how to use python using IDLE
+
+
+firstname = input("Enter your fisrt name please:")
+lastname = input("Enter your last name please:")
+print ("hello", firstname,lastname,"welcome to CTI_110")
