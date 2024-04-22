fc4a241b95cbbc3b2b6c128c52326c4cce8170fd
nesh8383 <166833846+nesh8383@users.noreply.github.com>
2024-04-21 23:04:27 -0400

    Add files via upload

---
+++ P2HW1_DenishiaWiliams.py
@@ -0,0 +1,45 @@
+#Denishia Williams
+#4/21/24
+#P2HW1
+#This assignment is a budget on travel expenses
+
+
+
+
+
+
+print("This program calculates and displays travel expenses")
+budget = float(input("Enter budget:"))
+location = (input("Enter your travel destination:"))
+gas = float(input('How much do you think you will spend on gas?'))
+accomodation = float(input("Approxiamately, how much will you need for accomodation/hotel?"))
+food = float(input("Last, how much do you need for food?"))
+
+
+
+
+
+
+
+
+
+
+
+print("------------ Travel Expenses ------------")
+print(f"Location:             Asheville ")
+print(f"Initial Budget:       $1200.0")
+print(f"fuel:                 $250.0")
+print(f"Accomodation:         $300.0")
+print(f"food:                 $200.0")
+print("-----------------------------------------")
+
+
+
+print("Remaining balance:", budget-gas-accomodation-food)
+
+
+
+
+
+ 
+ 
