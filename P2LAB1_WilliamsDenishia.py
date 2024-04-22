b33fa18f09dd52182f11bfcf17cd736255782420
nesh8383 <166833846+nesh8383@users.noreply.github.com>
2024-04-22 00:21:22 -0400

    Add files via upload

---
+++ P2LAB1_WilliamsDenishia.py
@@ -0,0 +1,29 @@
+# Denishia Williams
+# 4/20/2024
+# P2LAB1_
+#radius of the circle
+
+import math 
+
+
+CONSTANT_PI = 8.75
+
+radius = float(input("Enter radius of the circle: "))
+
+diameter=radius*2
+
+print(f"The diameter of circle is : {diameter}")
+
+import math
+
+circumference = radius*2*3.14
+
+print (f"the circumference of the circle:{circumference}")
+
+
+area = radius**2*3.141
+print(f"the area of the circle is : {area}")
+
+
+
+
