57eb9c0dcf6ecf793fa921e39620b469ae8fb15a
nesh8383 <166833846+nesh8383@users.noreply.github.com>
2024-05-05 23:40:18 -0400

    Add files via upload

---
+++ P4LAB1A_williams,Denishia.py
@@ -0,0 +1,36 @@
+
+#denishia williams
+#4/29/2024
+#Write a turtle graphics programs that draws a triangle and a square using loops.
+#P4LAB1
+
+
+
+
+
+import turtle
+
+win = turtle.Screen()
+
+alex = turtle.Turtle()
+
+for x in range (4):
+    alex.forward(100)
+    alex.left(90)
+
+
+
+alex.penup()
+alex.forward(120)
+alex.pendown()
+
+
+
+for _ in range(3):
+    alex.forward(100)
+    alex.left(120)
+
+
+
+
+
