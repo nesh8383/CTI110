0892891600ae7fb1e3292e715835f239e334cc17
nesh8383 <166833846+nesh8383@users.noreply.github.com>
2024-05-05 23:41:18 -0400

    Add files via upload

---
+++ P4LAB1B_WilliamsDenishia.py
@@ -0,0 +1,58 @@
+
+#Denishia Williams
+#5/03/2024
+#P4LAB1B
+#Draw your initials
+
+
+
+
+
+
+
+
+
+
+
+
+
+import turtle
+p=turtle.Turtle()
+p.penup()
+
+#draw straight line
+
+p.goto(-30,50)  
+p.pendown()
+p.pensize(10)
+p.pencolor("blue")
+p.right(90)
+p.forward(200)
+
+
+p.penup()
+p.goto(-30,50) #centering in the screen
+#draw  curves
+p.pendown()
+p.right(-90)
+p.circle(-100,180)
+
+
+
+
+
+t=turtle.Turtle()
+t.penup()
+t.goto(100,50)  
+t.pendown()
+t.pensize(10)
+t.pencolor("yellow")
+t.right(65)
+t.forward(100)
+t.left(130)
+t.forward(100)
+
+t.right(120)
+t.forward(100)
+t.left(130)
+t.forward(100)
